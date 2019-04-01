6e919ebecb60eef1633985b51fd31fd694ea7dcd
Payas17 <agustin.payaslian@gmail.com>
2019-04-01 17:31:51 -0300

    agrego arreglo de versiones

--- docker-entrypoint-heroku.sh
+++ docker-entrypoint-heroku.sh
@@ -1,3 +1,9 @@
+
+alembic stamp 737c7b27b23c
+python /code/src/hypechat/manage.py db init
+chmod -R 777 migrations
+python /code/src/hypechat/manage.py db migrate
 python /code/src/hypechat/manage.py db upgrade
+
 
 python /code/src/hypechat/main.py
