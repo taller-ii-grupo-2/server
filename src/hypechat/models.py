107f774ddc28ea4eee2ab177f43556ddc2973f89
Matias Rozanec <rozanecm@gmail.com>
2019-04-01 10:28:02 -0300

    revert changes to stage where everything worked

--- src/hypechat/models.py
+++ src/hypechat/models.py
@@ -1,12 +1,5 @@
-"""
-Define models used in the server
-"""
-from sqlalchemy import exc
-# pylint: disable = no-name-in-module
-from __init__ import db
+from init import db
 
-
-# pylint: disable = missing-docstring
 class Name(db.Model):
     __tablename__ = 'names'
 
@@ -17,25 +10,25 @@ class Name(db.Model):
     def __init__(self, name, last_name):
         self.name = name
         self.last_name = last_name
-
+        
     def __repr__(self):
         return '<id {}>'.format(self.id)
-
+    
     def serialize(self):
         return {
-            'id': self.id,
+            'id': self.id, 
             'name': self.name,
             'last_name': self.last_name,
         }
 
-    def add_name(self, name, last_name):
+    def add_name(name, last_name):
         try:
-            name = Name(
+            name=Name(
                 name=name,
                 last_name=last_name
             )
-            self.db.session.add(name)
-            self.db.session.commit()
+            db.session.add(name)
+            db.session.commit()
             return "Name added. name id={}".format(name.id)
-        except exc.SQLAlchemyError as exception:
-            return str(exception)
+        except Exception as e:
+            return(str(e))
