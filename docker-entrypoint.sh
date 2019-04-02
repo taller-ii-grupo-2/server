a8c6f94e2a302ba08c77dfcc4b81f31a6a265592
Matias Rozanec <rozanecm@gmail.com>
2019-04-02 00:21:09 -0300

    remove literally everything

--- docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-rm -rf migrations
-python /code/src/hypechat/manage.py db init
-chmod -R 777 migrations
-python /code/src/hypechat/manage.py db migrate
-python /code/src/hypechat/manage.py db upgrade
-
-python /code/src/hypechat/main.py
