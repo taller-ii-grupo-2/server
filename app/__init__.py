8426927f71493ae161c895222fd3d29adbbd7762
Payas17 <agustin.payaslian@gmail.com>
2019-04-14 17:44:51 -0300

    armo rest api

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,16 @@
 from flask import Flask
 from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+api = Api(app)
 
 from app import routes, models  # noqa: E402, F401
+
+api.add_resource(routes.Index, '/')
+api.add_resource(routes.Android, '/android')
+api.add_resource(routes.AllUsers, '/users/all')
+api.add_resource(routes.AddUsers, '/users/add')
--- app/routes.py
+++ app/routes.py
@@ -1,40 +1,47 @@
 """File containing all endpoints in the app."""
 from flask import request, jsonify
-from app import app
+from flask_restful import Resource
 from app.models import User  # pylint: disable = syntax-error
 
 
-@app.route('/')
-@app.route('/index')
-def index():
+class Index(Resource):
     """home endpoint"""
-    return "Hello, World!"
+    @classmethod
+    def get(cls):
+        """get mmethod"""
+        return "Hello, World!"
 
 
-@app.route('/android')
-def android():
-    """home endpoint"""
-    return "Hello, android"
+class Android(Resource):
+    """android endpoint"""
+    @classmethod
+    def get(cls):
+        """get mmethod"""
+        return "Hello, android"
 
 
-@app.route("/getall")
-def get_all():
-    """get all data in database"""
-    try:
-        users = User.query.all()
-        return jsonify([e.serialize() for e in users])
-    except Exception as exception:  # pylint: disable = broad-except
-        return str(exception)
+class AllUsers(Resource):
+    """all users endpoint"""
+    @classmethod
+    def get(cls):
+        """get mmethod"""
+        try:
+            users = User.query.all()
+            return jsonify([e.serialize() for e in users])
+        except Exception as exception:  # pylint: disable = broad-except
+            return str(exception)
 
 
-@app.route("/add")
-def add_user():
-    """add object to db"""
-    name = request.args.get('name')
-    last_name = request.args.get('last_name')
-    age = request.args.get('age')
-    mail = request.args.get('mail')
-    pais = request.args.get('pais')
-    token = request.args.get('token')
-    height = request.args.get('height')
-    return User.add_user(name, last_name, age, mail, pais, token, height)
+class AddUsers(Resource):
+    """add users endpoint"""
+    @classmethod
+    def post(cls):
+        """post method"""
+        name = request.args.get('name')
+        last_name = request.args.get('last_name')
+        age = request.args.get('age')
+        mail = request.args.get('mail')
+        pais = request.args.get('pais')
+        token = request.args.get('token')
+        height = request.args.get('height')
+        return User.add_user(name, last_name, age, mail, pais, token, height)
