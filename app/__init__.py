ab63675fe85f65e6e1b8ba1cd72f60b2abd41fcc
Payas17 <payas17@hotmail.com>
2019-04-19 17:05:28 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -17,4 +17,4 @@ from app import routes, users  # noqa: E402, F401
 api.add_resource(routes.Index, '/')
 api.add_resource(routes.Android, '/android')
 api.add_resource(routes.AllUsers, '/users/all')
-api.add_resource(routes.AddUsers, '/users/add')
+api.add_resource(routes.AddUsers, '/register')
