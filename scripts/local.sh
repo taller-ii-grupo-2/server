1c78477cf7428bfa5427e5bbef43c25b34e4b16f
Matias Rozanec <rozanecm@gmail.com>
2019-04-22 18:38:22 -0300

    update db script with db merge command

--- scripts/local.sh
+++ scripts/local.sh
@@ -1,5 +1,6 @@
 python /code/manage.py db init
 python /code/manage.py db migrate
 python /code/manage.py db upgrade
+python /code/manage.py db merge
 
 gunicorn --worker-class eventlet --log-level debug --bind 0.0.0.0:5000 app:app
